78e1f6f60061e4b20d4d0640fa48914603059be4
Sachinrn28 <121843899+Sachinrn28@users.noreply.github.com>
2023-09-21 23:04:42 +0530

    Python Certification Preparation
    Python Certification Preparation

---
+++ Assignment 1.1.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1.What are the primitive data types in Python?
+# - Data types which are created inbuilt are called as premitive data type or fundamental data types are called primitive data type.
+# = Example: int, float, str, bool
+
+# ## 2.Explain the difference between mutable and immutable data types?
+# - Data types which can be changed after creation are called as mutable data type
+#   - Example=Lists
+# - Data types which can not be changed ones they created are called are called as immutable data type.
+#   - Example=Tuple
+
+# ## 3.What is type casting in Python, and why is it important?
+# 
+# Type casting is converting one data type to another. It's important for operations where data types must match for compatibility.
+# 
+
+# ## 4.What are the common data types used in JavaScript?
+# 
+# 
+# Number, String, Boolean, Object, Array, Null, Undefined.
+# 
+
+# ## 5.Explain the concept of dynamic typing in programming languages.
+# 
+# Dynamic typing allows variables to change their data type during runtime without explicit type declarations.
+
+# ## 6.What are the three main types of conditional statements in Python?
+# if, elif, else.
+# 
+# ## 7.What is the difference between 'for' and 'while' loops?
+# 'for' loops are used for definite iteration, while 'while' loops are used for indefinite iteration.
+# 
+# ## 8.Explain the 'break' and 'continue' statements in Python.
+# 
+# 'break' is used to exit a loop prematurely, while 'continue' skips the current iteration and continues to the next.
+
+# ## 9.What is a function in programming, and why is it useful?
+# A function is a reusable block of code that performs a specific task. It promotes code reusability and modularity.
+
+# ## 10. What is the difference between parameters and arguments in a function?
+# 
+# Parameters are variables defined in the function's signature, while arguments are the values passed when calling the function.
+
+# ## 11.What is a recursive function, and when should you use one?
+# A recursive function is a function that calls itself. It's used when a problem can be broken down into smaller, similar subproblems.
+
+# ## 12.What is a lambda function (anonymous function) in Python?
+# A lambda function is a small, anonymous function defined using the lambda keyword, often used for short, simple operations.
+
+# ## 13.What is the purpose of the 'return' statement in a function?
+# 
+# The 'return' statement is used to specify the value that the function should produce as its result.
+
+# ## 14.What is function overloading, and does Python support it?
+# Function overloading is defining multiple functions with the same name but different parameter lists. Python does not support it directly.
+
+# ## 15.In Python, what is the purpose of the 'global' keyword within a function?
+# The 'global' keyword is used to declare a variable as global, allowing it to be accessed and modified outside the function's local scope.
+
+# ## 16.What is a higher-order function in functional programming, and why is it significant?
+# A higher-order function is a function that takes one or more functions as arguments or returns a function as its result. They enable more concise and expressive code.
+
+# ## 17.What is an infinite loop, and how can you avoid it in programming?
+# An infinite loop is a loop that runs indefinitely. To avoid it, ensure there's a condition that eventually becomes false or use control statements like 'break' or 'return'.
+
+# ## 18.Explain the 'elif' statement in Python and when to use it.
+# 'elif' is short for "else if" and is used to specify an alternative condition in an if-elif-else statement chain.
+
+# ## 19.Explain the concept of operator precedence and associativity.
+# Operator precedence determines the order in which operators are evaluated, and associativity resolves the order when operators have the same precedence.
+
+# ## 20.Explain the concept of type inference in programming languages like Python.
+# Type inference is the ability of a language to automatically deduce the data type of a variable based on its value.
